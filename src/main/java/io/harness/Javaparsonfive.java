9f6ac11e0d34afeab5373dd70102de6f554d7a57
GitHubApp <>
2025-05-19 13:26:22 +0000

    Project init

---
+++ src/main/java/io/harness/Javaparsonfive.java
@@ -0,0 +1,11 @@
+package io.harness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaparsonfive {
+    public static void main(String[] args) {
+        SpringApplication.run(Javaparsonfive.class, args);
+    }
+}
